213f73d8b0fef1cfe1aa4a95fadda4645594a306
a.kuzmin <anton.kuzmin@marketgid.com>
2016-09-17 09:33:21 +0300

    copyrights & comments

--- application/models/Common.php
+++ application/models/Common.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package Analytical Center 2
- * @see for US http://hack4sec.pro/wiki/index.php/Analytical_Center_2_en
+ * @see for EN http://hack4sec.pro/wiki/index.php/Analytical_Center_2_en
  * @see for RU http://hack4sec.pro/wiki/index.php/Analytical_Center_2
  * @license MIT
- * @copyright (c) Anton Kuzmin <http://anton-kuzmin.ru> (ru) <http://anton-kuzmin.pro> (us)
+ * @copyright (c) Anton Kuzmin <http://anton-kuzmin.ru> (ru) <http://anton-kuzmin.pro> (en)
  * @author Anton Kuzmin
  */
 class Common extends Zend_Db_Table_Abstract {
--- application/models/Tasks.php
+++ application/models/Tasks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Analytical Center 2
- * @see for US http://hack4sec.pro/wiki/index.php/Analytical_Center_2_en
+ * @see for EN http://hack4sec.pro/wiki/index.php/Analytical_Center_2_en
  * @see for RU http://hack4sec.pro/wiki/index.php/Analytical_Center_2
  * @license MIT
  * @copyright (c) Anton Kuzmin <http://anton-kuzmin.ru> (ru) <http://anton-kuzmin.pro> (en)
