e1ee865e23d74d351ff96307921b19f6e64b0d5a
a.kuzmin <anton.kuzmin@marketgid.com>
2016-10-01 10:36:57 +0300

    Unknown OS, alg name fix, fields fix, etc

--- dbchanges-2.0a_to_2.1a.sql
+++ dbchanges-2.0a_to_2.1a.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `hashes` CHANGE `salt` `salt` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
 ALTER TABLE `hashes` CHANGE `password` `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+INSERT INTO `os` (`name`) VALUES ('Unknown OS');
+UPDATE `hash_algs` SET `name` = 'md5(md5($pass))' WHERE `name`='md5(md5($pass)';
